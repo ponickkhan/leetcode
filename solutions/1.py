2f14b9c6ed2e0f6b7de8a33385d5b2745f407755
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-08 23:00:50 +0600

    two sum

---
+++ solutions/1.py
@@ -0,0 +1,12 @@
+#Two Sum
+
+class Solution:
+    def twoSum(self, nums, target):
+
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
