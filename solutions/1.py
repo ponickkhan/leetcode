c4484d85c98eee37f3d4671fdad31ee2f2189c54
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-11 18:44:42 +0600

    two sum context

--- solutions/1.py
+++ solutions/1.py
@@ -1,20 +1,31 @@
 # Two Sum
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# Example:
+#
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
 
+nums = [2, 7, 11, 15]
+target = 17
 class Solution:
     def twoSum(self, nums, target):
-
-        h = {}
+        hm = {}
         for i, num in enumerate(nums):
             n = target - num
-            if n not in h:
-                h[num] = i
+            if n not in m:
+                m[num] = i
+                print(m)
             else:
-                return [h[n], i]
+                return [m[n], i]
+
 
 # Example
 
 test = Solution()
-nums = [2, 7, 11, 15]
-target = 17
 result = test.twoSum(nums,target)
 print(result)
