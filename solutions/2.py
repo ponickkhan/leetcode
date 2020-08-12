8cabb227f3354b073484082582006bbf58ee8c5d
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2020-08-12 18:41:40 +0600

    add two numbers update

--- solutions/2.py
+++ solutions/2.py
@@ -10,10 +10,10 @@
 # Output: 7 -> 0 -> 8
 # Explanation: 342 + 465 = 807.
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def addTwoNumbers(self, l1, l2, c=0):
         """
@@ -33,3 +33,7 @@ class Solution:
             ret.next = self.addTwoNumbers(l1.next, l2.next, c)
         return ret
 
+
+test = Solution()
+result = test.addTwoNumbers([0,1],[0,1,2])
+print(result)
